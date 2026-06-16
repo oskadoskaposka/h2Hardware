daf2bf0257bfb6998fe982db1c9e3ba789abd491
Alexandre Paduan <68298933+oskadoskaposka@users.noreply.github.com>
2026-06-16 10:53:04 -0300

    Load catalog card size styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import "./catalog-size.css";
 import styles from "../styles/layout.module.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
