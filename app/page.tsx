e7a5dc9243e424cd705779c7c9ee6d4231afab2d
Alexandre Paduan <68298933+oskadoskaposka@users.noreply.github.com>
2026-06-19 01:37:25 -0300

    Apply catalog menu settings on home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 import CatalogExperience from "../components/CatalogExperience";
+import CatalogMenuAdjustments from "../components/CatalogMenuAdjustments";
 
 export default function HomePage() {
-  return <CatalogExperience />;
+  return (
+    <>
+      <CatalogMenuAdjustments />
+      <CatalogExperience />
+    </>
+  );
 }
