72b45771895137d84cd5861ec334bdb0b95f9f07
Alexandre Paduan <68298933+oskadoskaposka@users.noreply.github.com>
2026-07-01 01:06:28 -0300

    Move catalog PDF card below catalog

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,8 @@ export default function HomePage() {
   return (
     <>
       <CatalogMenuAdjustments />
-      <CatalogPdfDownloadCard />
       <CatalogExperience />
+      <CatalogPdfDownloadCard />
     </>
   );
 }
