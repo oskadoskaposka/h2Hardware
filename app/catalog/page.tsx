9d7484256218d2ee3870329c94b7c320be8adad0
Alexandre Paduan <68298933+oskadoskaposka@users.noreply.github.com>
2026-07-01 00:54:05 -0300

    Show catalog PDF download card

--- app/catalog/page.tsx
+++ app/catalog/page.tsx
@@ -1,10 +1,12 @@
 import CatalogExperience from "../../components/CatalogExperience";
 import CatalogMenuAdjustments from "../../components/CatalogMenuAdjustments";
+import CatalogPdfDownloadCard from "../../components/CatalogPdfDownloadCard";
 
 export default function CatalogPage() {
   return (
     <>
       <CatalogMenuAdjustments />
+      <CatalogPdfDownloadCard />
       <CatalogExperience />
     </>
   );
